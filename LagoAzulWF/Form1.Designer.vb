8f618091d8525ca22451df24446c89ecdb6fd636
tomitaf <felipe.tomita@gmail.com>
2020-06-03 19:17:38 -0300

    Versao 1.0 - Inicio do projeto Grande Lago Azul Piscinas

---
+++ LagoAzulWF/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.CadastroToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ConsultasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClienteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProdutoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PedidoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClientesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProdutosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PedidosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PessoaFísicaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PessoaJurídicaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PessoaFísicaToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PessoaJurídicaToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TodosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CadastroToolStripMenuItem, Me.ConsultasToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'CadastroToolStripMenuItem
+        '
+        Me.CadastroToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClienteToolStripMenuItem, Me.ProdutoToolStripMenuItem, Me.PedidoToolStripMenuItem})
+        Me.CadastroToolStripMenuItem.Name = "CadastroToolStripMenuItem"
+        Me.CadastroToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.CadastroToolStripMenuItem.Text = "Cadastro"
+        '
+        'ConsultasToolStripMenuItem
+        '
+        Me.ConsultasToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClientesToolStripMenuItem, Me.ProdutosToolStripMenuItem, Me.PedidosToolStripMenuItem})
+        Me.ConsultasToolStripMenuItem.Name = "ConsultasToolStripMenuItem"
+        Me.ConsultasToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.ConsultasToolStripMenuItem.Text = "Consulta"
+        '
+        'ClienteToolStripMenuItem
+        '
+        Me.ClienteToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PessoaFísicaToolStripMenuItem, Me.PessoaJurídicaToolStripMenuItem})
+        Me.ClienteToolStripMenuItem.Name = "ClienteToolStripMenuItem"
+        Me.ClienteToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ClienteToolStripMenuItem.Text = "Cliente"
+        '
+        'ProdutoToolStripMenuItem
+        '
+        Me.ProdutoToolStripMenuItem.Name = "ProdutoToolStripMenuItem"
+        Me.ProdutoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ProdutoToolStripMenuItem.Text = "Produto"
+        '
+        'PedidoToolStripMenuItem
+        '
+        Me.PedidoToolStripMenuItem.Name = "PedidoToolStripMenuItem"
+        Me.PedidoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PedidoToolStripMenuItem.Text = "Pedido"
+        '
+        'ClientesToolStripMenuItem
+        '
+        Me.ClientesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PessoaFísicaToolStripMenuItem1, Me.PessoaJurídicaToolStripMenuItem1, Me.TodosToolStripMenuItem})
+        Me.ClientesToolStripMenuItem.Name = "ClientesToolStripMenuItem"
+        Me.ClientesToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ClientesToolStripMenuItem.Text = "Clientes"
+        '
+        'ProdutosToolStripMenuItem
+        '
+        Me.ProdutosToolStripMenuItem.Name = "ProdutosToolStripMenuItem"
+        Me.ProdutosToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ProdutosToolStripMenuItem.Text = "Produtos"
+        '
+        'PedidosToolStripMenuItem
+        '
+        Me.PedidosToolStripMenuItem.Name = "PedidosToolStripMenuItem"
+        Me.PedidosToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PedidosToolStripMenuItem.Text = "Pedidos"
+        '
+        'PessoaFísicaToolStripMenuItem
+        '
+        Me.PessoaFísicaToolStripMenuItem.Name = "PessoaFísicaToolStripMenuItem"
+        Me.PessoaFísicaToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PessoaFísicaToolStripMenuItem.Text = "Pessoa Física"
+        '
+        'PessoaJurídicaToolStripMenuItem
+        '
+        Me.PessoaJurídicaToolStripMenuItem.Name = "PessoaJurídicaToolStripMenuItem"
+        Me.PessoaJurídicaToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PessoaJurídicaToolStripMenuItem.Text = "Pessoa Jurídica"
+        '
+        'PessoaFísicaToolStripMenuItem1
+        '
+        Me.PessoaFísicaToolStripMenuItem1.Name = "PessoaFísicaToolStripMenuItem1"
+        Me.PessoaFísicaToolStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.PessoaFísicaToolStripMenuItem1.Text = "Pessoa Física"
+        '
+        'PessoaJurídicaToolStripMenuItem1
+        '
+        Me.PessoaJurídicaToolStripMenuItem1.Name = "PessoaJurídicaToolStripMenuItem1"
+        Me.PessoaJurídicaToolStripMenuItem1.Size = New System.Drawing.Size(180, 22)
+        Me.PessoaJurídicaToolStripMenuItem1.Text = "Pessoa Jurídica"
+        '
+        'TodosToolStripMenuItem
+        '
+        Me.TodosToolStripMenuItem.Name = "TodosToolStripMenuItem"
+        Me.TodosToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.TodosToolStripMenuItem.Text = "Todos"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Grande Lago Azul Piscinas - Versão 1.0"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents CadastroToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ConsultasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClienteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProdutoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PedidoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClientesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProdutosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PedidosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PessoaFísicaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PessoaJurídicaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PessoaFísicaToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents PessoaJurídicaToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents TodosToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ LagoAzulWF/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub ConsultasToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ConsultasToolStripMenuItem.Click
+
+    End Sub
+End Class
